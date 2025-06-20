46763c40db80dced0b72d8eec016c4e47f32249a
ab2rasoul <a73rasoul@gmail.com>
2025-06-20 16:16:12 +0330

    contents

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from website.views import index_view, about_view, contact_view
 
+app_name = "website"
+
 urlpatterns = [
     path('index/', index_view),
     path('about/', about_view),

67a7025fac114b758b851d1ad5a2ee76cdd8ff47
ab2rasoul <a73rasoul@gmail.com>
2025-07-07 02:11:18 +0330

    working on templates and urls and views
    some changes in base.html
    changing addresses

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from blog.views import *
 app_name = "blog"
 urlpatterns = [
-    path('blog/', blog_view, name = 'index'),
-    path('blog/single', blog_single, name = 'single'),
-    path('test', test, name = 'test'),
+    path('', blog_view, name = 'index'),
+    path('<int:pid>', blog_single, name = 'single'),
+    # path('post-<int:pid>', test, name = 'test'),
 ]

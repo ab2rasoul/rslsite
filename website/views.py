c3cd871a0bea5952a11f623b256e6565cfa734a3
ab2rasoul <a73rasoul@gmail.com>
2025-07-21 14:43:24 +0330

    ewwriting contact

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from website.models import Contact
 from website.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -25,6 +25,9 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, "Your ticket submitted successfully")
+        else:
+            messages.add_message(request, messages.ERROR, "Your ticket submitted with failure") 
     form = ContactForm()
     return render(request,'website/contact.html', {'form':form})
 
